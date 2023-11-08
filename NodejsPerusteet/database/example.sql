b4579679238db58a491b646c4737538fa31104d8
Kamilla <KamillaKa@users.noreply.github.com>
2023-11-08 20:14:04 +0200

    Created example.sql

---
+++ NodejsPerusteet/database/example.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS mediashare;
+CREATE DATABASE mediashare;
+USE mediashare;
+
+-- Create tables
+CREATE TABLE Users (
+  user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  user_level_id INT NOT NULL,
+  created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE MediaItems (
+  media_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  filename VARCHAR(255) NOT NULL,
+  filesize INT NOT NULL,
+  media_type VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  created_at TIMESTAMP NOT NULL,
+  PRIMARY KEY (media_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
