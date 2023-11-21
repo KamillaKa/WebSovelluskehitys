4c67723a257cbe84f6c95dd219732fcf4a1d926a
Kamilla <KamillaKa@users.noreply.github.com>
2023-11-21 16:42:50 +0200

    Assingment 4 done

--- NodejsPerusteet/src/controllers/media-controller.mjs
+++ NodejsPerusteet/src/controllers/media-controller.mjs
@@ -1,22 +1,16 @@
 import { fetchAllMedia } from "../models/media-model.mjs";
+import { addMedia } from "../models/media-model.mjs";
 
 const postMedia = async (req, res) => {
   console.log("uploaded file", req.file);
   console.log("uploaded form data", req.body);
   const { title, description, user_id } = req.body;
   const { filename, mimetype, size } = req.file;
-  const newId = mediaItems[0].media_id + 1;
   if (filename && title && user_id) {
-    const result = await addMedia({
-      user_id,
-      filename,
-      size,
-      mimetype,
-      title,
-      description,
-    });
+    const newMedia = { title, description, user_id, filename, mimetype, size };
+    const result = await addMedia(newMedia);
     res.status(201);
-    res.json({ message: "Media added", ...result });
+    res.json({ message: "New media item added", ...result });
   } else {
     res.sendStatus(400);
   }
