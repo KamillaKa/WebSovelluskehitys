32e83dc0159413ff5403738a7a049bd165c50a92
Kamilla <KamillaKa@users.noreply.github.com>
2023-12-05 20:16:32 +0200

    implemented SSL stuff

--- NodejsPerusteet/src/index.js
+++ NodejsPerusteet/src/index.js
@@ -1,3 +1,5 @@
+import https from "https";
+import fs from "fs";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -13,55 +15,44 @@ import {
 } from "./middleware/middlewares.mjs";
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 443;
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('view engine', 'pug');
-app.set('views', 'src/views')
-app.disable('x-powered-by');
-
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.disable("x-powered-by");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/docs', express.static(path.join(__dirname, '../docs')));
-// Serve uploaded mediafiles url: /media/{file}
-app.use('/media', express.static(path.join(__dirname, '../uploads')));
+app.use(express.urlencoded({ extended: true }));
+app.use("/docs", express.static(path.join(__dirname, "../docs")));
+app.use("/media", express.static(path.join(__dirname, "../uploads")));
+app.use("/forms", express.static(path.join(__dirname, "../test-forms")));
 
-// Serve uploaded mediafiles url: /media/{file}
-app.use('/forms', express.static(path.join(__dirname, '../test-forms')));
-
-// Logger middleware
 if (process.env.NODE_ENV !== "production") {
   app.use(logger);
 }
 
-// Root route
 app.get("/", (req, res) => {
   const values = { title: "REST API docs", message: "API documentation here" };
   res.render("index", values);
 });
 
-// Modular routes
-app.use("api/auth", authRouter);
+app.use("/api/auth", authRouter);
 app.use("/api/media", mediaRouter);
 app.use("/api/users", userRouter);
 app.use("/api/likes", likesRouter);
 app.use("/api/comments", commentsRouter);
 
-// Error handling - catch-all for unhandled routes
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-// Start the server
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "..", "cert", "server.key")),
+  cert: fs.readFileSync(path.join(__dirname, "..", "cert", "server.cert")),
+};
+
+https.createServer(options, app).listen(port, hostname, () => {
+  console.log(`Server running at https://${hostname}:${port}/`);
 });
-
-// const userRoutes = require("./userRoutes");
-
-// app.use(express.json());
-// app.use("/user", userRoutes);
-
-// export default app;
